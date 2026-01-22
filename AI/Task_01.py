e2c3e6d390f3228e0cb8ac6f21543b7c83e4d63b
Muhammad Soofiyan <98753121+so662@users.noreply.github.com>
2026-01-22 17:33:06 +0500

    Add files via upload

--- AI/Task_01.py
+++ AI/Task_01.py
@@ -1,1 +1,16 @@
 
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+age = int(input("Enter your age: "))
+users = {
+    "username": username,
+    "password": password,
+    "age": age
+}
+if age < 13:
+    print("Access denied: You must be at least 13 years old.")
+else:
+    print("Access granted: Welcome,", username + "!") 
+    print("User name : ", username)
+    print("User age : ", age)
+    print("User password : ", password) 
