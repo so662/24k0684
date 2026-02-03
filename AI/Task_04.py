a0a4126dd6160a60581572c24db5ea6423b014e6
Muhammad Soofiyan <98753121+so662@users.noreply.github.com>
2026-02-03 20:37:36 +0500

    Delete AI/Task_04.py Delete AI/Task_05.py

--- AI/Task_04.py
+++
@@ -1,7 +0,0 @@
-def salary(s):
-    HRA = s * 0.1
-    DA = s * 0.05
-    return s + HRA + DA
-s = float(input("Enter the basic salary: "))
-total_salary = salary(s)
-print("The total salary is:", total_salary)
